a86a2ea15cd95f03e46189d3df1062dae1124bba
Andrey <Andrey@electis.ru>
2022-12-07 16:00:56 +0700

    without cuda

--- create.py
+++ create.py
@@ -50,8 +50,9 @@ def get_playing_text(filepath):
 
 
 def create(conf: Config):
-    video_params = dict(hwaccel_output_format='cuda')
-    output_params = dict(acodec='aac', vcodec='h264_nvenc', f='flv', maxrate='2000k', bufsize='4000k', shortest=None)
+    video_params = dict()
+    # video_params = dict(hwaccel_output_format='cuda')
+    output_params = dict(acodec='aac', vcodec='h264', f='flv', maxrate='2000k', bufsize='4000k', shortest=None)
     output_params.update({
         'b:v': '1500k', 'b:a': '128k', 'ar': '44100', 'framerate': '30', 'g': '60',
         'force_key_frames': "expr:gte(t,n_forced*2)"
